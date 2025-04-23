3a29fc728e4a1e2d27bc394acc4929750909c7cf
elay-china <elay_china@163.com>
2025-04-23 16:54:27 +0800

    init

---
+++ zhang-common/zhang-common-log/src/main/java/org/zhang/common/log/enums/BusinessStatus.java
@@ -0,0 +1,18 @@
+package org.zhang.common.log.enums;
+
+/**
+ * 操作状态
+ *
+ * @author zhang
+ */
+public enum BusinessStatus {
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
